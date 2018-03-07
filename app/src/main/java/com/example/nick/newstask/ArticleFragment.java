d4e1fc6a3900c3135f1872d9888e44cf73f001d4
112Nick <mrnickrus@gmail.com>
2018-03-07 20:40:57 +0300

    orientation

--- app/src/main/java/com/example/nick/newstask/ArticleFragment.java
+++ app/src/main/java/com/example/nick/newstask/ArticleFragment.java
@@ -9,13 +9,31 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 
 public class ArticleFragment extends BaseFragment {
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.article_item_view, container, false);
+        Bundle bundle = this.getArguments();
+        View newsView =  inflater.inflate(R.layout.article_fragment, container, false);
+        if (bundle != null) {
+            String title = bundle.getString("title");
+            String date = bundle.getString("date");
+            String content = bundle.getString("content");
+
+            TextView dateTxt = newsView.findViewById(R.id.date);
+            TextView titleTxt = newsView.findViewById(R.id.title);
+            TextView contentTxt = newsView.findViewById(R.id.contentText);
+            dateTxt.setText(date);
+            titleTxt.setText(title);
+            contentTxt.setText(content);
+
+
+        }
+
+        return newsView;
     }
 }
--- app/src/main/java/com/example/nick/newstask/MainActivity.java
+++ app/src/main/java/com/example/nick/newstask/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.nick.newstask;
 
+import android.content.res.Configuration;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import ru.mail.park.articlelistlib.Article;
 import ru.mail.park.articlelistlib.ArticleListFragment;
@@ -15,19 +17,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         final FragmentManager manager = getSupportFragmentManager();
+
         final FragmentTransaction transaction = manager.beginTransaction();
-        ArticleListFragment list = new ArticleListFragment();
-        transaction.add(R.id.container, list);
-        transaction.commit();
-        list.setOnArticleClickListener(new OnArticleClickListener() {
-            @Override
-            public void onArticleClick(Article article) {
-                transaction.replace(R.id.container, new ArticleFragment());
-                transaction.commit();
-            }
-        });
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            ArticleListFragment list = new ArticleListFragment();
+            transaction.add(R.id.container, list);
+            transaction.commit();
+            list.setOnArticleClickListener(new OnArticleClickListener() {
+                @Override
+                public void onArticleClick(Article article) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("title", article.getTitle());
+                    bundle.putString("content", article.getContent());
+                    bundle.putString("date", article.getDate().toString());
+                    ArticleFragment articleFragment = new ArticleFragment();
+                    articleFragment.setArguments(bundle);
+                    final FragmentTransaction transaction2 = manager.beginTransaction();
+                    transaction2.replace(R.id.container, articleFragment);
+                    transaction2.addToBackStack(null);
+                    transaction2.commit();
+                }
+            });
+        } else {
+            ArticleListFragment list = new ArticleListFragment();
+            transaction.add(R.id.containerList, list);
+            transaction.commit();
+            list.setOnArticleClickListener(new OnArticleClickListener() {
+                @Override
+                public void onArticleClick(Article article) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("title", article.getTitle());
+                    bundle.putString("content", article.getContent());
+                    bundle.putString("date", article.getDate().toString());
+                    ArticleFragment articleFragment = new ArticleFragment();
+                    articleFragment.setArguments(bundle);
+                    final FragmentTransaction transaction2 = manager.beginTransaction();
+                    transaction2.add(R.id.containerArticle, articleFragment);
+                    transaction2.commit();
+                }
+            });
+        }
+
+
 
     }
 }
